2aaa85eb654de0bd29f5cedfbad8313449f2610f
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-21 16:15:07 +0530

    User Registration

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The User Registration Problem");
+            Registration user = new Registration();
+            Console.Write("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            user.Check(firstName);
+        }
+    }
+}
