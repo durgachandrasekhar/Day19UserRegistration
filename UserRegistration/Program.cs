1ebddd3058c3e7452cf95eb0d464b0ca6cf997f9
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-21 16:20:59 +0530

    Valid last name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,9 +8,13 @@ namespace UserRegistration
         {
             Console.WriteLine("The User Registration Problem");
             Registration user = new Registration();
-            Console.Write("Enter your first name : ");
+            Console.Write("Enter your First name : ");
             string firstName = Console.ReadLine();
             user.Check(firstName);
+            Console.Write("Enter your Last name : ");
+            string LastName = Console.ReadLine();
+            user.Check(LastName);
+
         }
     }
 }
